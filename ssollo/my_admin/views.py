46c4a9b8b850181dfeae6ff4f5c8933bfb16a1f2
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-06-04 17:38:19 +0300

    Delete ssollo directory

--- ssollo/my_admin/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def admin_start(request):
-    return render(request, 'my_admin/start_admin.html')
--- ssollo/user_and_company/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Person
-from .models import Company
-
-
-admin.site.register(Person)
-admin.site.register(Company)
